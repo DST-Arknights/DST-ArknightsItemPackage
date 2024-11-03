e8dd05c6ac1d957d1c4c858d0701f90a05e4da74
望月心灵 <TohsakaKuro@outlook.com>
2024-11-03 23:52:01 +0800

    fix: optimize currency check and adjust UI padding for better layout

--- scripts/modmain/ark_currency.lua
+++ scripts/modmain/ark_currency.lua
@@ -65,9 +65,9 @@ end)
 AddClassPostConstruct('components/inventory_replica', function(self)
   local _Has = self.Has
   self.Has = function(self, item, amount, ...)
-    for k, _ in pairs(TUNING.ARK_CURRENCY_TYPES) do
-      if item == k then
-        local left = self.inst.replica.ark_currency:GetArkCurrencyByType(k)
+    for _, type in pairs(TUNING.ARK_CURRENCY_TYPES) do
+      if item == type then
+        local left = self.inst.replica.ark_currency:GetArkCurrencyByType(type)
         return left >= amount, left
       end
     end
--- scripts/modmain/ark_item_ui.lua
+++ scripts/modmain/ark_item_ui.lua
@@ -5,12 +5,10 @@ table.insert(Assets, Asset("ATLAS", "images/ark_ui/icon_gold.xml"))
 table.insert(Assets, Asset("ATLAS", "images/ark_ui/icon_diamond.xml"))
 table.insert(Assets, Asset("ATLAS", "images/ark_ui/icon_diamond_shd.xml"))
 
-local PADDING = 4;
-local RIGHT_PADDING = 80;
+local PADDING = 6;
+local RIGHT_PADDING = 280;
 
 local screenSize = nil
-local cacheUiW = nil
-local cacheUiH = nil
 local function positionCurrencyUI(controls)
     local curScreenSize = {TheSim:GetScreenSize()}
     if screenSize and screenSize[1] == curScreenSize[1] and screenSize[2] == curScreenSize[2] then
@@ -20,23 +18,20 @@ local function positionCurrencyUI(controls)
     local hudScale = controls.top_root:GetScale()
     local screenW = curScreenSize[1] / hudScale.x
     local screenH = curScreenSize[2] / hudScale.y
-    if not cacheUiW or not cacheUiH then
-        local originUiW, originUiH = controls.arkCurrency.bg:GetSize()
-        local scale = controls.arkCurrency.bg:GetScale()
-        cacheUiW = originUiW * scale.x
-        cacheUiH = originUiH * scale.y
-    end
-    local positionX = screenW / 2 - RIGHT_PADDING - cacheUiW / 2 - PADDING
-    local positionY = - cacheUiH / 2 - PADDING
+
+    local originUiW, originUiH = controls.arkCurrency.bg:GetSize()
+    local scale = controls.arkCurrency.bg:GetScale()
+    local positionX = screenW / 2 - RIGHT_PADDING - originUiW / 2 - PADDING
+    local positionY = - originUiH / 2 - PADDING
     controls.arkCurrency:SetPosition(positionX, positionY, 0)
     local miniMap = controls.minimap_small
     if miniMap then
         local x, y, z = miniMap:GetPosition():Get()
         local mapH = miniMap.mapsize.h
         local mapW = miniMap.mapsize.w
-        local maxMapY = - cacheUiH - 2 * PADDING - mapH / 2
-        local minMapX = positionX - cacheUiW / 2 - PADDING - mapW / 2
-        local maxMapX = positionX + cacheUiW / 2 + PADDING + mapW / 2
+        local maxMapY = - originUiH - 2 * PADDING - mapH / 2
+        local minMapX = positionX - originUiW / 2 - PADDING - mapW / 2
+        local maxMapX = positionX + originUiW / 2 + PADDING + mapW / 2
         if maxMapY < y and minMapX < x and maxMapX > x then
             miniMap:SetPosition(x, maxMapY, z)
         end
