80e1d9f081b0c6131d94982c9b1a14b867186836
望月心灵 <TohsakaKuro@outlook.com>
2026-02-16 15:06:26 +0800

    feat: 添加标题和描述字段到ArkBuffIcon，更新状态同步逻辑

--- scripts/components/ark_buff_icon.lua
+++ scripts/components/ark_buff_icon.lua
@@ -10,17 +10,28 @@ end
 local function ontex(self, value)
   self.inst.replica.ark_buff_icon.state.tex = value
 end
+local function ontitle(self, value)
+  self.inst.replica.ark_buff_icon.state.title = value
+end
+local function ondesc(self, value)
+  self.inst.replica.ark_buff_icon.state.desc = value
+end
+
 local ArkBuffIcon = Class(function(self, inst)
   self.inst = inst
   self.totalTime = 0
   self.remainingTime = 0
   self.atlas = ''
   self.tex = ''
+  self.title = ''
+  self.desc = ''
 end, nil, {
   totalTime = ontotalTime,
   remainingTime = onremainingTime,
   atlas = onatlas,
   tex = ontex,
+  title = ontitle,
+  desc = ondesc,
 })
 
 function ArkBuffIcon:SetTexture(atlas, tex)
@@ -34,6 +45,14 @@ end
 
 function ArkBuffIcon:SetRemainingTime(remainingTime)
   self.remainingTime = remainingTime
+end
+
+function ArkBuffIcon:SetTitle(title)
+  self.title = title
+end
+
+function ArkBuffIcon:SetDesc(desc)
+  self.desc = desc
 end
 
 function ArkBuffIcon:AttachTo(target)
