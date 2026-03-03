75ba1fe18aa73e5dcbcb175c14eea6aaf6488f7d
望月心灵 <TohsakaKuro@outlook.com>
2026-03-03 23:57:22 +0800

    feat: 添加新的工作坊和训练室原型图像及其相关文件

--- modmain/ark_tech.lua
+++ modmain/ark_tech.lua
@@ -37,38 +37,38 @@ for i, v in pairs(AllRecipes) do
 end
 
 AddPrototyperDef('ark_workshop', {
-  icon_atlas = "images/ark_item_prototyper.xml",
-  icon_image = "ark_item_prototyper.tex",
+  icon_atlas = "images/ark_workshop_prototyper.xml",
+  icon_image = "ark_workshop_prototyper.tex",
   is_crafting_station = true,
   action_str = 'ARK_WORKSHOP',
   filter_text = STRINGS.UI.CRAFTING_FILTERS.ARK_WORKSHOP
 })
 
+RegisterInventoryItemAtlas("images/ark_workshop.xml", "ark_workshop.tex")
 -- 制造站
 AddRecipe2('ark_workshop', {
   Ingredient('charcoal', 10),
 }, TECH.SCIENCE_TWO, {
   placer = 'ark_workshop_placer',
-  atlas = "images/ark_item_prototyper.xml",
-  image = "ark_item_prototyper.tex",
 })
 AddRecipeToFilter("ark_workshop", "PROTOTYPERS")
 
 -- 训练室
 AddPrototyperDef('ark_training_room', {
-  icon_atlas = "images/ark_item_prototyper.xml",
-  icon_image = "ark_item_prototyper.tex",
+  icon_atlas = "images/ark_training_room_prototyper.xml",
+  icon_image = "ark_training_room_prototyper.tex",
   is_crafting_station = true,
   action_str = 'ARK_TRAINING_ROOM',
   filter_text = STRINGS.UI.CRAFTING_FILTERS.ARK_TRAINING_ROOM
 })
+
+RegisterInventoryItemAtlas("images/ark_training_room.xml", "ark_training_room.tex")
+
 AddRecipe2("ark_training_room",
   {Ingredient("boards", 4), Ingredient("goldnugget", 2)},
   TECH.SCIENCE_TWO,
   {
     placer = 'ark_training_room_placer',
-    atlas = "images/ark_item_prototyper.xml",
-    image = "ark_item_prototyper.tex",
   }
 )
 AddRecipeToFilter("ark_training_room", "PROTOTYPERS")
