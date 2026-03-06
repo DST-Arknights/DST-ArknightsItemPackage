d3fee90d35ef17020f2f97d7e1b3899118c3614c
望月心灵 <TohsakaKuro@outlook.com>
2026-03-07 00:25:47 +0800

    feat: 更新版本号至0.0.9

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ QQ群: 666511586
 欢迎大家积极参与!]]
 })
 author = "让 望月心灵"
-version = "0.0.8"
+version = "0.0.9"
 forumthread = "https://steamcommunity.com/sharedfiles/filedetails/?id=3677284770"
 
 api_version = 10
