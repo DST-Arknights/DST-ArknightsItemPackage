6129e3faa9e88f878ba625ba0d2dfdfef291d731
望月心灵 <TohsakaKuro@outlook.com>
2026-04-04 12:19:15 +0800

    release: v1.1.0

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ QQ群: 666511586
 欢迎大家积极参与!]]
 })
 author = "让 望月心灵"
-version = "1.0.12"
+version = "1.1.0"
 forumthread = "https://steamcommunity.com/sharedfiles/filedetails/?id=3677284770"
 
 api_version = 10
