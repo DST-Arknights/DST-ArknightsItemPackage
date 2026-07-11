7865a068a8d5f207cf4b5647bc2efec0cd73b800
望月心灵 <TohsakaKuro@outlook.com>
2026-07-11 12:52:40 +0800

    重构Talker类的语音数据解析逻辑，优化Play方法，简化代码结构

--- scripts/components/i18n_talker.lua
+++ scripts/components/i18n_talker.lua
@@ -31,66 +31,64 @@ local function ResolveText(inst, string_path)
 end
 
 
+-- 在全局语音表中按 key 查找 string_path
+-- 优先当前语言，回退到该 key 下的第一个语言
+local function TryResolveVoiceFromKey(key, string_path, voice_lang)
+  local lang_tbl = GLOBAL_SOUND_MAP[key]
+  if not lang_tbl then return nil end
+  local lang_map = lang_tbl[voice_lang]
+  if lang_map and lang_map[string_path] then
+    return lang_map[string_path]
+  end
+  local first_lang = next(lang_tbl)
+  if first_lang then
+    local first_lang_map = lang_tbl[first_lang]
+    if first_lang_map and first_lang_map[string_path] then
+      return first_lang_map[string_path]
+    end
+  end
+end
+
+function Talker:ResolveVoiceData(string_path)
+  -- 1. 优先查 SetupVoice 设置的 key
+  local data = self._voice_key and TryResolveVoiceFromKey(self._voice_key, string_path, self.voice_lang)
+  if data then return data end
+  -- 2. 扩散查找全局表第一个可用 key
+  for key in pairs(GLOBAL_SOUND_MAP) do
+    data = TryResolveVoiceFromKey(key, string_path, self.voice_lang)
+    if data then return data end
+  end
+end
+
 function Talker:Play(string_path, params)
   if not string_path then return end
   params = params or {}
-  params.sgparam = params.sgparam or {}
-  local sound_data = nil
-  -- 1. 优先查 SetupVoice 设置的 key
-  if self._voice_key and GLOBAL_SOUND_MAP[self._voice_key] then
-    local lang_map = GLOBAL_SOUND_MAP[self._voice_key][self.voice_lang]
-    if lang_map and lang_map[string_path] then
-      sound_data = lang_map[string_path]
+  local sound = params.sound ~= false
+  local talk = params.talk ~= false
+  if not sound and not talk then return end
+  local interrupt = params.interrupt ~= false
+  local talk_params = params.talker_params or {}
+  local sound_params = params.sound_params or {}
+  talk_params.sgparam = talk_params.sgparam or {}
+  -- 仅当需要播放声音 或 需要 duration 回退时才查表
+  local sound_data = (sound or not talk_params.time) and self:ResolveVoiceData(string_path) or nil
+  if sound and sound_data and sound_data.path and self.inst.SoundEmitter then
+    local name = sound_data.name or string_path
+    if interrupt then
+      self.inst.SoundEmitter:KillSound(name)
     end
-    -- 若当前语言找不到，尝试该key下的第一个语言
-    if not sound_data then
-      local first_lang = next(GLOBAL_SOUND_MAP[self._voice_key])
-      if first_lang then
-        local first_lang_map = GLOBAL_SOUND_MAP[self._voice_key][first_lang]
-        if first_lang_map and first_lang_map[string_path] then
-          sound_data = first_lang_map[string_path]
-        end
-      end
-    end
+    self.inst.SoundEmitter:PlaySound(sound_data.path, name, sound_params.volume, sound_params.ispredicted)
+    talk_params.sgparam.played_by_i18n_talker = true
   end
-  -- 2. 若没找到，扩散查找全局表第一个可用key
-  if not sound_data then
-    for key, lang_tbl in pairs(GLOBAL_SOUND_MAP) do
-      local lang_map = lang_tbl[self.voice_lang]
-      if lang_map and lang_map[string_path] then
-        sound_data = lang_map[string_path]
-        break
-      end
-      -- 查第一个语言
-      local first_lang = next(lang_tbl)
-      if not sound_data and first_lang then
-        local first_lang_map = lang_tbl[first_lang]
-        if first_lang_map and first_lang_map[string_path] then
-          sound_data = first_lang_map[string_path]
-          break
-        end
-      end
-    end
-  end
-  if not sound_data then
-    ArkLogger:Warn("Talker:Play sound_key not found", string_path)
-  elseif self.inst.SoundEmitter then
-    if not params.overlap then
-      self.inst.SoundEmitter:KillSound(string_path)
-    end
-    self.inst.SoundEmitter:PlaySound(sound_data.path, string_path)
-    params.sgparam.played_by_i18n_talker = true
-  end
-  ArkLogger:Debug("Talker: notext", string_path, params.notext)
-  if not params.notext then
+  if talk then
     local text = self.text_cache_map[string_path]
     if not text then
       text = ResolveText(self.inst, string_path)
       self.text_cache_map[string_path] = text
     end
     if text and self.inst.components.talker then
-      local time = params.time or (sound_data and sound_data.duration)
-      self.inst.components.talker:Say(text, time, params.noanim, params.force, params.nobroadcast, params.colour, params.text_filter_context, params.original_author_netid, params.onfinishedlinesfn, params.sgparam)
+      local time = talk_params.time or (sound_data and sound_data.duration)
+      self.inst.components.talker:Say(text, time, talk_params.noanim, talk_params.force, talk_params.nobroadcast, talk_params.colour, talk_params.text_filter_context, talk_params.original_author_netid, talk_params.onfinishedlinesfn, talk_params.sgparam)
     end
   end
 end
